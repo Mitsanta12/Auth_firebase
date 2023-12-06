3543e189bef38958a9d5163ec2933d1144a23889
Mitsanta_ANDRIAH <hei.mitsanta@gmail.com>
2023-12-06 11:47:18 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+import React, { useEffect, useState } from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Profile from './Profile'
+import Register from './Register'
+import VerifyEmail from './VerifyEmail';
+import {AuthProvider} from './AuthContext'
+import {auth} from './firebase'
+import {onAuthStateChanged} from 'firebase/auth'
+import PrivateRoute from './PrivateRoute'
+import {Navigate} from 'react-router-dom'
+
+function App() {
+
+  const [currentUser, setCurrentUser] = useState(null)
+  const [timeActive, setTimeActive] = useState(false)
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user)
+    })
+  }, [])
+
+  return (
+<Router>
+  {/* Fournit les informations d'authentification aux composants enfants */}
+  <AuthProvider value={{ currentUser, timeActive, setTimeActive }}>
+    <Routes>
+      {/* Route pour la page d'accueil */}
+      <Route exact path='/' element={
+        /* Utilisation d'une route privée pour la page de profil */
+        <PrivateRoute>
+          {/* Composant de la page de profil */}
+          <Profile/>
+        </PrivateRoute>
+      }/>
+      {/* Route pour la page de connexion */}
+ 
+      {/* Route pour la page d'inscription */}
+      <Route path="/register" element={
+        // Vérifie si l'utilisateur est connecté et a vérifié son email, sinon redirige vers la page d'accueil
+        !currentUser?.emailVerified 
+          ? <Register/> // Composant de la page d'inscription
+          : <Navigate to='/' replace/> // Redirection vers la page d'accueil
+      } />
+      {/* Route pour la page de vérification d'email */}
+      <Route path='/verify-email' element={<VerifyEmail/>} /> 
+    </Routes>  
+  </AuthProvider>
+</Router>
+
+  );
+}
+
+export default App;
+/**/
